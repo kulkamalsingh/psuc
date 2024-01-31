97e151513d5cba0c37636b053949aa9a829577c7
Kulkamal Singh <kulkamalsingh377@gmail.com>
2024-01-31 22:31:44 +0530

    .

---
+++ Experiment15/2_drawCircle.c
@@ -0,0 +1,26 @@
+// 2. Write C program to draw circle.
+
+// #include <graphics.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    // int gd = DETECT, gm, x, y, r;
+
+    printf("Enter the center coordinates of the circle: ");
+    // scanf("%d %d", &x, &y);
+
+    printf("Enter the radius of the circle: ");
+    // scanf("%d", &r);
+
+    // initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+
+    // Drawing the circle
+    // circle(x, y, r);
+
+    getch();
+    closegraph();
+    return 0;
+}
